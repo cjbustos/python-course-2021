fa370a9d88931d4a5529cd09f2b22cfaef5a1920
Carlos Bustos <bustos_carlos_j@hotmail.com>
2023-01-01 12:10:50 -0300

    Named functions and lambda functions

---
+++ functions.py
@@ -0,0 +1,47 @@
+from statistics import mean
+# mypy functions.py
+
+# Example1 - Functions
+def calculate(args):
+    return mean(args)
+
+scores = [7, 9, 10]
+average = calculate(scores)
+
+# Example2 - Functions with Docstring - help(max) show Docstring
+def max(a, b):
+    """ Docstring - This function calculate max between two values, both must be integer """
+    if a > b:
+        max_value = a
+    else:
+        max_value = b
+    return max_value
+
+# Example3 - Functions with types declared    
+def min(a: int, b: int) -> int:
+    """ Docstring - This function calculate min between two values, both must be integer """
+    if a < b:
+        min_value = a
+    else:
+        min_value = b
+    return min_value
+
+print(average)
+print(max(2, 3))
+print(min(7, 5))
+
+# Example4 - Lambda functions
+# Long
+prices_in_dollar = [20, 15.5, 215]
+prices_in_pesos = []
+
+for price_in_dollar in prices_in_dollar:
+    prices_in_pesos.append(float(price_in_dollar) * 346)
+
+# Short
+prices_in_pesos = list(map(
+    lambda dollar: float(dollar) * 346,
+    prices_in_dollar
+))
+
+print(prices_in_pesos)
